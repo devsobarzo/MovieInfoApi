f97f83c069fde22011a7103a9c14621996f2929d
sobarzorober <sobarzorober@gmail.com>
2022-09-29 02:49:34 -0300

    wip:4 Presentation

--- app/src/main/java/cl/rober/movieinfoapi/presentation/MovieViewModel.kt
+++ app/src/main/java/cl/rober/movieinfoapi/presentation/MovieViewModel.kt
@@ -2,8 +2,25 @@ package cl.rober.movieinfoapi.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import cl.rober.movieinfoapi.core.Resource
+import cl.rober.movieinfoapi.repository.MovieRepository
 import kotlinx.coroutines.Dispatchers
 
-class MovieViewModel  {
+class MovieViewModel(private val repo: MovieRepository): ViewModel() {
 
+    fun fetchMovies() = liveData(Dispatchers.IO) {
+        emit(Resource.Loading())
+        try {
+            emit(Resource.Success(repo.getMovies()))
+        } catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+    }
+}
+
+class MovieViewModelFactory(private val repo: MovieRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(MovieRepository::class.java).newInstance(repo)
+    }
 }
